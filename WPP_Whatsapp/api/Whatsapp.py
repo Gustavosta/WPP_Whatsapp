725831273ddbcdb53ff335c4ddac7d2cf9cfddd6
Gustavo Santana <43628256+Gustavosta@users.noreply.github.com>
2023-09-23 01:02:16 -0300

    Update Whatsapp.py

--- WPP_Whatsapp/api/Whatsapp.py
+++ WPP_Whatsapp/api/Whatsapp.py
@@ -1,5 +1,5 @@
-import threading
-import time
+import asyncio
+import inspect
 
 from WPP_Whatsapp.api.layers.HostLayer import HostLayer
 from WPP_Whatsapp.api.const import defaultOptions, Logger
@@ -8,58 +8,53 @@ from WPP_Whatsapp.api.layers.ListenerLayer import ListenerLayer
 
 
 class Whatsapp(BusinessLayer):
-    interval: threading.Event
+    connected = None
 
-    def __init__(self, session, threadsafe_browser, **kwargs):
-        self.connected = None
-        self.options = {}
-        # self.autoCloseInterval = None
-        self.autoCloseCalled = False
-        self.isInitialized = False
-        self.isInjected = False
-        self.isStarted = False
-        self.isLogged = False
-        self.isInChat = False
-        # self.checkStartInterval = None
-        self.urlCode = ''
-        self.status = ''
-        self.attempt = 0
-        self.lastPercent = ""
-        self.lastPercentMessage = ""
+    def __init__(self, session, threadsafe_browser, *args, **kwargs):
         self.session = session
         self.page = threadsafe_browser.page
         self.browser = threadsafe_browser.browser
         self.ThreadsafeBrowser = threadsafe_browser
+        # self.loop = kwargs.get("loop")  # or asyncio.new_event_loop()
+        # if not self.loop:
+        #     raise Exception("Not Add Loop")
+        # asyncio.set_event_loop(self.loop)
+        self.session = session
         self.options.update(defaultOptions)
         self.logger = self.options.get("logger") or Logger
         self.logger.info(f'{self.session}: Initializing...')
         self.logQR = kwargs.get("logQR") or False
-        self.autoClose = kwargs.get("autoClose") or self.options.get("autoClose") or 60
+        self.autoClose = kwargs.get("autoClose") or self.options.get("autoClose") or 0
         HostLayer.__init__(self)
         ListenerLayer.__init__(self)
         self.handel()
 
     def handel(self):
-        # self.interval = None
+        self.interval = None
         if self.page:
-            self.interval = self.__setInterval(self.__intervalHandel, 60)
             self.page.on('close', lambda: self.clearInterval(self.interval))
+
+        self.interval = self.__setInterval(self.__intervalHandel, 60)
 
     @staticmethod
     def __setInterval(func, interval, *args, **kwargs):
-        stopped = threading.Event()
+        loop = kwargs.get('loop') or asyncio.get_event_loop()
+        stopped = asyncio.Event()
 
-        def _loop_():
+        async def loop_():
             while not stopped.is_set():
-                func()
-                time.sleep(interval)
+                if inspect.iscoroutinefunction(func):
+                    await func()
+                    await asyncio.sleep(interval)
+                else:
+                    func()
+                    await asyncio.sleep(interval)
 
-        th = threading.Thread(target=_loop_)
-        th.start()
+        loop.create_task(loop_())
         return stopped
 
-    def __intervalHandel(self):
-        newConnected = self.ThreadsafeBrowser.sync_page_evaluate("() => WPP && WPP.conn.isRegistered()")
+    async def __intervalHandel(self):
+        newConnected = self.page_evaluate("() => WPP && WPP.conn.isRegistered()")
 
         if newConnected is None or newConnected == self.connected:
             return
@@ -67,7 +62,8 @@ class Whatsapp(BusinessLayer):
 
         if not newConnected:
             self.logger.info(f'{self.session}: Session Unpaired')
-            self.statusFind('disconnectedMobile', self.session)
+            # asyncio.sleep(1)
+            self.statusFind('desconnectedMobile', self.session)
 
     async def afterPageScriptInjected(self):
         await self._afterPageScriptInjectedHost()
@@ -84,7 +80,7 @@ class Whatsapp(BusinessLayer):
     async def getMessageById(self, messageId):
         return self.ThreadsafeBrowser.page_evaluate("(messageId) => WAPI.getMessageById(messageId)", messageId)
 
-    async def getMessages(self, chatId, params=None):
+    def getMessages(self, chatId, params=None):
         """
         :param chatId:
         :param params: (count, id, direction)
@@ -93,7 +89,7 @@ class Whatsapp(BusinessLayer):
         if not params:
             params = {}
         chatId = self.valid_chatId(chatId)
-        return self.ThreadsafeBrowser.page_evaluate("({ chatId, params }) => WAPI.getMessages(chatId, params)",
+        return self.ThreadsafeBrowser.sync_page_evaluate("({ chatId, params }) => WAPI.getMessages(chatId, params)",
                                                     {"chatId": chatId, "params": params})
 
     async def rejectCall(self, callId):
